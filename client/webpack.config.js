804f3a54f75946fbb0cd44248be24e1b5ed7b6d4
elizabeth padron <elizabethpadron02@gmail.com>
2024-03-30 18:18:57 -0500

    updates to files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,83 +7,69 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-
-plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        chunks: ['main']
-      }),
-      new HtmlWebpackPlugin({
-        template: './src/install.html',
-        filename: 'install.html',
-        chunks: ['install']
-      }),
-
-      new WebpackPwaManifest({
-        name: 'Your App Name',
-        short_name: 'Your App Short Name',
-        description: 'Your app description',
-        background_color: '#ffffff',
-        theme_color: '#2196f3',
-        icons: [
+    return {
+      mode: 'development',
+      entry: {
+        main: './src/js/index.js',
+        install: './src/js/install.js'
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          filename: 'index.html',
+          chunks: ['main']
+        }),
+        new HtmlWebpackPlugin({
+          template: './src/install.html',
+          filename: 'install.html',
+          chunks: ['install']
+        }),
+  
+        new WebpackPwaManifest({
+          name: 'Your App Name',
+          short_name: 'Your App Short Name',
+          description: 'Your app description',
+          background_color: '#ffffff',
+          theme_color: '#2196f3',
+          icons: [
+            {
+              src: path.resolve('src/images/icon.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons')
+            }
+          ]
+        }),
+  
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'sw.js',
+          exclude: [/\.map$/, /manifest\.json$/, /index\.html$/, /install\.html$/],
+        }),
+      ],
+  
+      module: {
+        rules: [
           {
-            src: path.resolve('src/images/icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons')
-          }
-        ]
-      }),
-
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'sw.js',
-        exclude: [/\.map$/, /manifest\.json$/, /index\.html$/, /install\.html$/],
-      }),
-
-      new WorkboxPlugin.GenerateSW({
-        clientsClaim: true,
-        skipWaiting: true,
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp('^https://your-api-url.com/'),
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-            },
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
           },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
-   
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
+  
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
             }
           }
-        }
-      ],
-    },
+        ],
+      },
+    };
   };
-};
+  
